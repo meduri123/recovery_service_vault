3c2d8adbac1e35fb2ec3cffe13466a8ee8c7552c
srikanth meduri <srikanthmeduri1224@gmail.com>
2025-05-02 19:25:12 +0530

    updated

--- terraform.tf
+++ terraform.tf
@@ -19,12 +19,12 @@ terraform {
     }
   }
 }
-provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
+# provider "azurerm" {
+#   features {
+#     resource_group {
+#       prevent_deletion_if_contains_resources = false
+#     }
+#   }
 #   subscription_id = var.subscription_id
 #   tenant_id       = var.tenant_id
- }
+#  }
